b47015de0e8ae7b5c91ab434437e5a11790eb5a6
Marcos Eckart <mhme2000@hotmail.com>
2023-09-24 14:55:06 -0400

    Init Web server

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [javax.servlet/servlet-api "2.5"]
+                 [ring "1.9.0"]
+                 [metosin/reitit "0.5.12"]
+                 [metosin/muuntaja "0.6.8"]
+                 [com.taoensso/carmine "3.2.0"]]
   :main ^:skip-aot url-shortener.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/url_shortener/core.clj
+++ src/url_shortener/core.clj
@@ -1,7 +1,27 @@
 (ns url-shortener.core
+  (:require [ring.adapter.jetty :as ring-jetty]
+            [reitit.ring :as ring]
+            [muuntaja.core :as m]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [clojure.set :as set]
+            [taoensso.carmine :as car :refer [wcar]])
   (:gen-class))
 
+(defn health-check-handler [arg]
+  {:status 200
+   :body "URL shortener"})
+(def app
+  (ring/ring-handler
+    (ring/router
+      ["/"
+       ["" {:get health-check-handler}]]
+      {:data {:muuntaja m/instance
+              :middleware [muuntaja/format-middleware]}})))
+(defn start-web-server []
+  (ring-jetty/run-jetty app {:port 3000
+                             :join? false}))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Function main to start web server"
   [& args]
-  (println "Hello, World!"))
+  (start-web-server))
